849a6b2c9ed7daf44fc548b65539933d57cf9eaf
Llisha-Bleu <66648340+Llisha-Bleu@users.noreply.github.com>
2020-08-08 04:16:54 -0400

    Updating schema.sql file and adding retirement_info.csv file.

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -66,3 +66,54 @@ SELECT * FROM dept_manager;
 SELECT * FROM employees;
 SELECT * FROM salaries;
 SELECT * FROM titles;
+
+--Drop/Delete Table
+DROP TABLE employees CASCADE;
+DROP TABLE departments CASCADE;
+DROP TABLE dept_manager CASCADE;
+DROP TABLE dept_emp CASCADE;
+DROP TABLE titles CASCADE;
+DROP TABLE salaries CASCADE;
+
+--Determine Retirement Eligibility (between 1952 and 1955)
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+--Determine Retirement Eligibility (born in 1952)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+--Determine Retirement Eligibility (born in 1953)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+--Determine Retirement Eligibility (born in 1954)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+--Determine Retirement Eligibility (born in 1955)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create NEW table - retirement_info to save queried results
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Query retirement_info table
+SELECT * FROM retirement_info;
